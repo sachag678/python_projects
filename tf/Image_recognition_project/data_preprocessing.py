530099bb757ea4c72e0ddfe9ab055e1b5c1e496d
sachag678 <sacha.gunaratne@gmail.com>
2017-06-18 16:57:06 -0400

    added the scikit-learn onehotencoder() module for the encoder

--- tf/Image_recognition_project/data_preprocessing.py
+++ tf/Image_recognition_project/data_preprocessing.py
@@ -2,7 +2,7 @@
 import scipy.io as sio
 from skimage.color import rgb2grey
 import numpy as np
-
+from sklearn.preprocessing import OneHotEncoder
 
 def normalize(images):
 	"""This function normalizes the image files and returns normalized images(0 mean and unit variance)"""
@@ -27,21 +27,25 @@ def one_hot_encode_labels(batch, num_classes):
 	   if y[0:1] = 2 && num_classes = 6:
 	   		then y[0:1] = [0,0,1,0,0,0] 
 	"""
-	labels = np.zeros((len(batch),num_classes))
-	for j in range(len(batch)):
-		for i in range(num_classes):
-			if(batch[j]==[i]):
-				labels[j][i-1]=1
+	# labels = np.zeros((len(batch),num_classes))
+	# for j in range(len(batch)):
+	# 	for i in range(num_classes):
+	# 		if(batch[j]==[i]):
+	# 			labels[j][i-1]=1
 
-	return labels
+	# return labels
+	encoder = OneHotEncoder()
+	return encoder.fit_transform(batch).toarray()
+
 
 if __name__ =='__main__':
 	images,labels = load_data('test_32x32.mat')
 	ohc_labels = one_hot_encode_labels(labels,10)
 	print(ohc_labels.shape)
-	normalized_images = normalize(images)
-	print(normalized_images.shape)
-	print(normalized_images.dtype)
+	#normalized_images = normalize(images)
+	#print(normalized_images)
+	#print(normalized_images.shape[0])
+	#print(normalized_images.dtype)
 	# new_normalized = np.float32(normalized_images)
 	# print(type(new_normalized[1,1,1,1]))
 	# print(new_normalized.shape)
