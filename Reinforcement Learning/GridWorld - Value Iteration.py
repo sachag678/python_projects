f6f9b88ca450b1f158355efa7c7af76413c28f2f
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2017-12-06 11:55:14 -0500

    added commens and docstrings

--- Reinforcement Learning/GridWorld - Value Iteration.py
+++ Reinforcement Learning/GridWorld - Value Iteration.py
@@ -4,6 +4,15 @@ size = 3
 board = np.zeros((size,size))
 
 def update(size, transition_reward, board, i,j):
+	'''Updates a specific location by looking at 4 directions and following the bellman optimality equation
+	params: size - the size of the board
+			transition_reward - the reward for moving to a new state
+			board - the current board
+			i - location on the board
+			j - location on the board
+	return:
+			the max value of the updated value function using bellmans optimality
+	'''
 	if(i==0 and j==0):
 		return 0
 	if(i==0 and j!=size-1):
@@ -57,6 +66,13 @@ def update(size, transition_reward, board, i,j):
 	return 1
 
 def evaluate(transition_reward,size,board):
+	'''Updates a board once
+	params: size - the size of the board
+			transition_reward - the reward for moving to a new state
+			board - the current board 
+	return: 
+			a fully updated board
+	'''
 	new_board = np.zeros((size,size))
 	for i in range(size):
 		for j in range(size):
@@ -64,6 +80,14 @@ def evaluate(transition_reward,size,board):
 	return new_board
 
 def run(transition_reward,size, cycles):
+	'''Runs the update for a set number of cycles and returns the board either when the cycles are over or it 
+	has converged. 
+	params: size - the size of the board
+			transition_reward - the reward for moving to a new state
+			cycles - the number of iterations to update
+	return:
+			the optimal policy
+	'''
 	board = np.zeros((size,size))
 	for i in range(cycles):
 		updated_board = evaluate(transition_reward,size,board)
